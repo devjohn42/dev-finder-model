268a9787ffd9aeb26b555f887bf53f8463b8d1cd
devjohn42 <devjohncb@gmail.com>
2026-06-27 19:15:00 -0300

    :sparkles: feat: creates the header

--- src/components/global/header.tsx
+++ src/components/global/header.tsx
@@ -1,24 +1,65 @@
 'use client'
 
+import { LogIn, LogOut } from 'lucide-react'
+import Link from 'next/link'
 import { signIn, signOut, useSession } from 'next-auth/react'
 import { ModeToggle } from '@/components/mode-toggle'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Button } from '@/components/ui/button'
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuGroup,
+	DropdownMenuItem,
+	DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
 
-export function Header() {
+function AccountDropDown() {
 	const session = useSession()
+	const isLoggedIn = !!session.data
 
 	return (
-		<header>
-			<div>
-				{session.data ? (
-					<Button onClick={() => signOut()}>Sign Out</Button>
-				) : (
-					<Button onClick={() => signIn('google')}>Sign In</Button>
-				)}
-				<ModeToggle />
+		<DropdownMenu>
+			<DropdownMenuTrigger>
+				<Button>
+					<Avatar className="mr-2">
+						<AvatarImage src={session.data?.user.image ?? ''} />
+						<AvatarFallback>JV</AvatarFallback>
+					</Avatar>
+
+					{session.data?.user.name}
+				</Button>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent>
+				<DropdownMenuGroup>
+					{isLoggedIn ? (
+						<DropdownMenuItem onClick={() => signOut()}>
+							<LogOut className="mr-2" /> Sign Out
+						</DropdownMenuItem>
+					) : (
+						<DropdownMenuItem onClick={() => signIn('google')}>
+							<LogIn className="mr-2" />
+							Sign In
+						</DropdownMenuItem>
+					)}
+				</DropdownMenuGroup>
+			</DropdownMenuContent>
+		</DropdownMenu>
+	)
+}
+
+export function Header() {
+	return (
+		<header className="border-b bg-background px-4 py-4">
+			<div className="flex items-center justify-between">
+				<Link href="/">Dev Finder Model</Link>
+				<div className="flex items-center gap-4">
+					<AccountDropDown />
+					<ModeToggle />
+				</div>
 			</div>
 		</header>
 	)
 }
 
-//32:14
+//1:16:02
--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,8 +1,7 @@
-'use-client'
+'use client'
 
 import { ThemeProvider as NextThemesProvider, ThemeProviderProps } from 'next-themes'
-import * as React from 'react'
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+	return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
